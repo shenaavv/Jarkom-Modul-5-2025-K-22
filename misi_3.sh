a1e4fdc1063f24ab9917a6a538c58cdf87e7e991
Kanafira Vanesha P <vaneshakath@gmail.com>
2025-12-02 16:56:14 +0700

    Update misi_3.sh

--- misi_3.sh
+++ misi_3.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-. ./config.sh
+sysctl -w net.ipv4.ip_forward=1
+iptables -F
+iptables -t nat -F
+iptables -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
 
 iptables -A INPUT -s 192.222.1.192/29 -j DROP
 iptables -A OUTPUT -d 192.222.1.192/29 -j DROP
